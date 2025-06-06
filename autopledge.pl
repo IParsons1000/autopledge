355e2091973b0aadc8f1045f74c9968bef5734c6
Ira Parsons <irap@cox.net>
2025-06-06 10:34:23 -0700

    Functioning poc; still buggy because pledge.com requires LD_PRELOAD which runs after promises are pledged

--- autopledge.pl
+++ autopledge.pl
@@ -4,7 +4,7 @@
 # autopledge.pl - automatically pledge syscall promises
 #
 
-use AutoPledge::Core;
+use AutoPledge::Core ':promises';
 use AutoPledge::Glibc;
 
 # process command line arguments
@@ -13,9 +13,13 @@ my $program = $ARGV[0];
 # detect syscalls needed
 my $promises = AutoPledge::Glibc::detect($program);
 
+# necessary to get working because pledge implementation is buggy:
+$promises |= RPATH;
+
 # convert promises into appropriate string for pledge
 my $arg = AutoPledge::Core::stringify($promises);
 
-exec('./pledge.com', '-p', $arg, $program);
+# run linux pledge port without warnings (-q) and without unveiling (-V)
+exec('./pledge.com', '-qVp', $arg, $program);
 
 exit;
