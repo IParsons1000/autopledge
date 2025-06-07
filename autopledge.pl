37883cc0ecbb1bd8d9696ec9b6d474fca9e3bf4f
Ira Parsons <irap@cox.net>
2025-06-06 21:24:09 -0700

    Beautification

--- autopledge.pl
+++ autopledge.pl
@@ -4,11 +4,17 @@
 # autopledge.pl - automatically pledge syscall promises
 #
 
+use Getopt::Std;
+
 use AutoPledge::Core ':promises';
 use AutoPledge::Glibc;
 use AutoPledge::Syscall;
 
 # process command line arguments
+getopts('') or die HELP_MESSAGE();
+if (@ARGV == 0) {
+    HELP_MESSAGE();
+};
 my $program = $ARGV[0];
 
 # detect syscalls needed
@@ -26,3 +32,15 @@ my $arg = AutoPledge::Core::stringify($promises);
 exec('./pledge.com', '-qVp', $arg, $program);
 
 exit;
+
+sub HELP_MESSAGE {
+    print 'Usage: ./autopledge.pl </path/to/binary>' . "\n";
+    exit;
+    return;
+};
+
+sub VERSION_MESSAGE {
+    print 'autopledge 0.2' . "\n";
+    exit;
+    return;
+};
