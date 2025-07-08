2407cf331e42c130162ccfbd3f355488f75a57a2
IParsons1000 <iparsons1000+github@gmail.com>
2025-07-08 14:23:58 -0700

    Detect terminal calls of glibc functions

--- tools/glibc-get-syscalls.py
+++ tools/glibc-get-syscalls.py
@@ -6,6 +6,7 @@
 #
 
 import angr
+import networkx
 
 # load glibc and linked libs
 p = angr.Project('/usr/lib64/libc.so.6', load_options={ 'auto_load_libs': True });
@@ -18,5 +19,10 @@ cfg = p.analyses.CFGFast();
 # recover function call graph from control flow graph
 cg = cfg.functions.callgraph;
 
-for addr in cg.nodes:
-	print(cfg.functions.function(addr).name);
+# create subgraph from function
+sg = cg.subgraph(networkx.single_source_shortest_path(cg, cfg.functions.function(name="_exit").addr).keys());
+
+# locate nodes at the end of the line
+for n, d in sg.out_degree():
+	if d == 0:
+		print(cfg.functions.function(n).name);
