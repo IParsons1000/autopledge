df308a6b91946bb1a9a8ce91833ff35438d9c5c4
IParsons1000 <iparsons1000+github@gmail.com>
2025-07-08 15:38:21 -0700

    Locate and enumerate syscalls needed by glibc functions

--- tools/glibc-get-syscalls.py
+++ tools/glibc-get-syscalls.py
@@ -19,10 +19,25 @@ cfg = p.analyses.CFGFast();
 # recover function call graph from control flow graph
 cg = cfg.functions.callgraph;
 
-# create subgraph from function
-sg = cg.subgraph(networkx.single_source_shortest_path(cg, cfg.functions.function(name="_exit").addr).keys());
+# find syscalls required for all outward-facing glibc functions
+for s in list(set(list(p.loader.symbols))):
 
-# locate nodes at the end of the line
-for n, d in sg.out_degree():
-	if d == 0:
-		print(cfg.functions.function(n).name);
+	if s.is_function and not s.is_local and cfg.functions.function(name=s.name):
+
+		# create subgraph from function
+		sg = cg.subgraph(networkx.single_source_shortest_path(cg, cfg.functions.function(name=s.name).addr).keys());
+
+		# locate nodes at the end of the line
+		calls = [];
+		for n, d in sg.out_degree():
+			if (d == 0) and cfg.functions.function(n).is_syscall:
+				calls.append(cfg.functions.function(n).name);
+
+		# convert syscall names to syscall.h macro format
+		for i, call in enumerate(calls):
+			calls[i] = "SYS_" + call;
+
+		# format for header file insertion
+		arrent = "{  \"" + s.name + "\" , (int *)&(int []){ " + ", ".join(calls) + ", -1 } }";
+
+		if calls: print(arrent);
